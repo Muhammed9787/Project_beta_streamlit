3b85dc55655b1845cf4615cf7481d57ac6e9381f
Muhammed9787 <79834550+Muhammed9787@users.noreply.github.com>
2021-09-25 15:37:49 +0200

    Update sir.py

--- pages/sir.py
+++ pages/sir.py
@@ -3,24 +3,24 @@
 import streamlit as st
 import mysql.connector
 def app():
-# Initialize connection.
-# Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
+    # Initialize connection.
+    # Uses st.cache to only run once.
+    @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+    def init_connection():
+        return mysql.connector.connect(**st.secrets["mysql"])
 
-conn = init_connection()
+    conn = init_connection()
 
-# Perform query.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+    # Perform query.
+    # Uses st.cache to only rerun when the query changes or after 10 min.
+    @st.cache(ttl=600)
+    def run_query(query):
+        with conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+    rows = run_query("SELECT * from mytable;")
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+    # Print results.
+    for row in rows:
+        st.write(f"{row[0]} has a :{row[1]}:")
