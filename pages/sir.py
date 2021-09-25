51b3ae631503e575363bbc1f68e20e3077c8b9b3
Muhammed9787 <79834550+Muhammed9787@users.noreply.github.com>
2021-09-25 15:30:35 +0200

    Update sir.py

--- pages/sir.py
+++ pages/sir.py
@@ -1,65 +1,26 @@
-import scipy
-from scipy.integrate import odeint
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import datetime
-from datetime import datetime
+# streamlit_app.py
+
 import streamlit as st
+import mysql.connector
 
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
 
-def app():
-################################################################################################################
+conn = init_connection()
 
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-  population = 30000000  # Population of the United States
+rows = run_query("SELECT * from mytable;")
 
-  # Initial conditions for infected and recovered people
-  initial_infected, initial_recovered = 1, 0
-
-  # Initial conditions for everyone else.
-  initial_everyone_else = population - initial_infected - initial_recovered
-
-  initial_conditions = initial_everyone_else, initial_infected, initial_recovered
-
-  n_days = 500  # Days over which to integrate
-  time = np.linspace(0, n_days, n_days)
-
-  # Contact Rate - We don't know this for coronavirus, so use it as a relative term for comparison.
-  contact_rate = 0.25
-  recovery_rate = 1/14  # Recovery Rate -
-
-  # The SIR model, integreated over 500 days.
-
-
-  def SIR(initial_conditions, t, population, contact_rate, recovery_rate):
-      S, I, R = initial_conditions
-      dS = -contact_rate*S*I/population
-      dI = contact_rate*S*I/population - recovery_rate*I
-      dR = recovery_rate*I
-      return dS, dI, dR
-
-
-  result = odeint(SIR, initial_conditions, time, args=(
-      population, contact_rate, recovery_rate))
-  S, I, R = result.T
-
-
-  # What happens if we're in contact with fewer people?
-  contact_rate = st.slider('Contact rate', 0.10, 1.0, 0.30, 0.05)
-
-
-  result = odeint(SIR, initial_conditions, time, args=(
-      population, contact_rate, recovery_rate))
-  S, I, R = result.T
-
-  fig = plt.figure(facecolor='w')
-  ax = fig.add_subplot(111, axisbelow=True)
-  ax.plot(time, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
-  ax.plot(time, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
-  ax.plot(time, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
-  ax.set_xlabel('Days')
-  ax.set_ylabel('Number (1000s)')
-  legend = ax.legend()
-  legend.get_frame().set_alpha(0.5)
-  st.write(fig)
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
