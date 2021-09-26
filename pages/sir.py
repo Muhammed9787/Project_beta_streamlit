a00c103c70c6c93e5d6dbe3842d201980edcf3bd
Muhammed9787 <79834550+Muhammed9787@users.noreply.github.com>
2021-09-26 19:34:54 +0200

    Update sir.py

--- pages/sir.py
+++ pages/sir.py
@@ -21,7 +21,7 @@ def app():
             return cur.fetchall()
 
     rows = run_query("SELECT * from mytable;")
-    df = pd.read_sql_query(rows)
+    df = pd.read_sql_query(rows,con=conn)
     st.write(df)
 
     # Print results.
